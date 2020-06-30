722bb467ce14358870e624c4b4fc88a16823a411
Finn Beckitt-Marshall <finn@pictura.co.uk>
2020-06-30 11:36:19 +0100

    Initial Commit

---
+++ ESP32 Environment Monitor/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ESP32 Environment Monitor
+//
+//  Created by Finn Beckitt-Marshall on 30/06/2020.
+//  Copyright © 2020 Finn Beckitt-Marshall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
